3284fdca21db47244b57913c59f03af282291a36
Teddi <83455454+teddinotteddy@users.noreply.github.com>
2022-08-27 09:20:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ const API_KEY = process.env.API_KEY;
 
 app.use( express.json() )
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(express.static(path.join(__dirname, "/public")))
 
 app.get("/", (req, res) => {
